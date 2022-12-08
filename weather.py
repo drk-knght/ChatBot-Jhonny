d21861f3922c8e1a78d06e0225046802f2c2f9f5
Om Agarwal <71601478+drk-knght@users.noreply.github.com>
2022-12-08 15:14:02 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,8 +2,8 @@ import os
 import requests
 import json
 import sys
-API_KEY="16f2f559e2b5457472a45571901"
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=16f2f559e2b5457472a4557190194f0c&units=metric'
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=16f&units=metric'
 def weather(address,query):
     resp=requests.get(url=url.format(address))
     data=resp.json()
